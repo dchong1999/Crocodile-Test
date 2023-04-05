476f795bab6a39ff98ea696eac3dcc78a99c2560
Diana Chong <dchong0123@gmail.com>
2023-04-05 00:03:33 -0400

    changed input type to password for sign in password field

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -18,7 +18,7 @@ const Signin = ({setPage, username, password}) => {
       <h1>Signin</h1>
       <form onSubmit={(e) => {handle(e)}}>
         <div><input type="text" placeholder="Username" onChange={(e) => {setUsernameAttempt(e.target.value)}}></input></div>
-        <div><input type="text" placeholder="Password" onChange={(e) => {setPasswordAttempt(e.target.value)}}></input></div>
+        <div><input type="password" placeholder="Password" onChange={(e) => {setPasswordAttempt(e.target.value)}}></input></div>
         {error ? <h4>Your account or password is incorrect. If you don't remember your password reset it now.</h4>: null}
         <div><button className='square' type='submit'>Login</button></div>
         <div><button className='round' type='button' onClick={(e) => setPage(2)}>Forgot Password</button></div>
